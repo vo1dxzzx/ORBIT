e7ed9311db1071cf698509b70e90a1c05290583e
vo1dxzzx <officialajtgaming@gmail.com>
2025-07-03 11:01:46 -0700

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
