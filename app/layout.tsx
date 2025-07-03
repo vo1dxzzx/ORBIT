d349f89dee04a2b7c906c960dbfc321bde921a28
vo1dxzzx <officialajtgaming@gmail.com>
2025-07-03 10:57:35 -0700

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,11 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "ORBIT",
   description:
-    "The ultimate Valorant community dedicated to perfecting Neon movement, advanced techniques, and lightning-fast gameplay."
-}
+    "The ultimate Valorant community dedicated to perfecting Neon movement, advanced techniques, and lightning-fast gameplay.",
+  icons: {
+    icon: "https://i.imgur.com/gl3A7h3.png", // <-- Paste your direct Imgur link here
+  },
+};
 
 export default function RootLayout({
   children,
